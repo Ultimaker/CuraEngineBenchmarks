ca762ac012ef049d3fbc4aac116f480f2e2bd289
github-action-benchmark <github@users.noreply.github.com>
2026-07-01 08:22:38 +0000

    add GCodeAnalyzer (customBiggerIsBetter) benchmark result for 14ec43297a1c8a7425c5f9dd3d4c454f73a37b58

--- dev/profiles_bench/data.js
+++ dev/profiles_bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1782894138056,
+  "lastUpdate": 1782894158694,
   "repoUrl": "https://github.com/Ultimaker/GCodeInsight",
   "entries": {
     "GCodeAnalyzer": [
@@ -90,6 +90,98 @@ window.BENCHMARK_DATA = {
           },
           {
             "name": "Slicing time UMMXL_arrow.gcode",
+            "value": 0,
+            "unit": "s"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "name": "Erwan MATHIEU",
+            "username": "wawanbreton",
+            "email": "erwan.mathieu@ultimaker.com"
+          },
+          "committer": {
+            "name": "Erwan MATHIEU",
+            "username": "wawanbreton",
+            "email": "erwan.mathieu@ultimaker.com"
+          },
+          "id": "14ec43297a1c8a7425c5f9dd3d4c454f73a37b58",
+          "message": "Try and get more info about publishing issue\n\nCURA-13226",
+          "timestamp": "2026-06-30T14:45:19Z",
+          "url": "https://github.com/Ultimaker/GCodeInsight/commit/14ec43297a1c8a7425c5f9dd3d4c454f73a37b58"
+        },
+        "date": 1782894144894,
+        "tool": "customBiggerIsBetter",
+        "benches": [
+          {
+            "name": "Print time UMS5_painted_cube.gcode",
+            "value": 3319.16545610311,
+            "unit": "s"
+          },
+          {
+            "name": "Microsegment violations in wall-skin UMS5_painted_cube.gcode",
+            "value": 6124,
+            "unit": "-"
+          },
+          {
+            "name": "Microsegment violations in infill UMS5_painted_cube.gcode",
+            "value": 124,
+            "unit": "-"
+          },
+          {
+            "name": "Number of retractions UMS5_painted_cube.gcode",
+            "value": 809,
+            "unit": "-"
+          },
+          {
+            "name": "Total travel length UMS5_painted_cube.gcode",
+            "value": 56867.400877555534,
+            "unit": "mm"
+          },
+          {
+            "name": "Minimum Line Length UMS5_painted_cube.gcode",
+            "value": 0.000999999999990564,
+            "unit": "mm"
+          },
+          {
+            "name": "Line Lengths 25 Percentile UMS5_painted_cube.gcode",
+            "value": 0.398664018943266,
+            "unit": "mm"
+          },
+          {
+            "name": "Minimum All Distances UMS5_painted_cube.gcode",
+            "value": 0.0010000000000047748,
+            "unit": "mm"
+          },
+          {
+            "name": "All Distances 25 Percentile UMS5_painted_cube.gcode",
+            "value": 0.4773782567314963,
+            "unit": "mm"
+          },
+          {
+            "name": "Extrusion-Axis UMS5_painted_cube.gcode",
+            "value": 0.03332,
+            "unit": "mm"
+          },
+          {
+            "name": "Extrusion Lengths 25 Percentile UMS5_painted_cube.gcode",
+            "value": 19.12068,
+            "unit": "mm"
+          },
+          {
+            "name": "Number Of Temperature Commands UMS5_painted_cube.gcode",
+            "value": 59512,
+            "unit": "#"
+          },
+          {
+            "name": "Mean Temperature UMS5_painted_cube.gcode",
+            "value": 205,
+            "unit": "Celcius"
+          },
+          {
+            "name": "Slicing time UMS5_painted_cube.gcode",
             "value": 0,
             "unit": "s"
           }
