7f7506bc26c22a5f39b464585864ccb60350b5ed
github-action-benchmark <github@users.noreply.github.com>
2026-07-01 08:53:06 +0000

    add GCodeAnalyzer UMMXL_arrow (customBiggerIsBetter) benchmark result for d04e0d809372363dfe9c7e4e74dc4cf7ad0f4e2e

---
+++ dev/profiles_bench/UMMXL_arrow/data.js
@@ -0,0 +1,100 @@
+window.BENCHMARK_DATA = {
+  "lastUpdate": 1782895985961,
+  "repoUrl": "https://github.com/Ultimaker/GCodeInsight",
+  "entries": {
+    "GCodeAnalyzer UMMXL_arrow": [
+      {
+        "commit": {
+          "author": {
+            "name": "Erwan MATHIEU",
+            "username": "wawanbreton",
+            "email": "erwan.mathieu@ultimaker.com"
+          },
+          "committer": {
+            "name": "Erwan MATHIEU",
+            "username": "wawanbreton",
+            "email": "erwan.mathieu@ultimaker.com"
+          },
+          "id": "d04e0d809372363dfe9c7e4e74dc4cf7ad0f4e2e",
+          "message": "Use official benchmark workflow again\n\nCURA-13266",
+          "timestamp": "2026-07-01T08:49:59Z",
+          "url": "https://github.com/Ultimaker/GCodeInsight/commit/d04e0d809372363dfe9c7e4e74dc4cf7ad0f4e2e"
+        },
+        "date": 1782895970535,
+        "tool": "customBiggerIsBetter",
+        "benches": [
+          {
+            "name": "Print time UMMXL_arrow.gcode",
+            "value": 5014.778564874503,
+            "unit": "s"
+          },
+          {
+            "name": "Microsegment violations in wall-skin UMMXL_arrow.gcode",
+            "value": 0,
+            "unit": "-"
+          },
+          {
+            "name": "Microsegment violations in infill UMMXL_arrow.gcode",
+            "value": 0,
+            "unit": "-"
+          },
+          {
+            "name": "Number of retractions UMMXL_arrow.gcode",
+            "value": 1310,
+            "unit": "-"
+          },
+          {
+            "name": "Total travel length UMMXL_arrow.gcode",
+            "value": 14984.513224862869,
+            "unit": "mm"
+          },
+          {
+            "name": "Minimum Line Length UMMXL_arrow.gcode",
+            "value": 0.01699999999999946,
+            "unit": "mm"
+          },
+          {
+            "name": "Line Lengths 25 Percentile UMMXL_arrow.gcode",
+            "value": 0.676689736880941,
+            "unit": "mm"
+          },
+          {
+            "name": "Minimum All Distances UMMXL_arrow.gcode",
+            "value": 0.016000000000000014,
+            "unit": "mm"
+          },
+          {
+            "name": "All Distances 25 Percentile UMMXL_arrow.gcode",
+            "value": 0.3999999999999999,
+            "unit": "mm"
+          },
+          {
+            "name": "Extrusion-Axis UMMXL_arrow.gcode",
+            "value": 0.51375,
+            "unit": "mm"
+          },
+          {
+            "name": "Extrusion Lengths 25 Percentile UMMXL_arrow.gcode",
+            "value": 1051.62525,
+            "unit": "mm"
+          },
+          {
+            "name": "Number Of Temperature Commands UMMXL_arrow.gcode",
+            "value": 30331,
+            "unit": "#"
+          },
+          {
+            "name": "Mean Temperature UMMXL_arrow.gcode",
+            "value": 255,
+            "unit": "Celcius"
+          },
+          {
+            "name": "Slicing time UMMXL_arrow.gcode",
+            "value": 0,
+            "unit": "s"
+          }
+        ]
+      }
+    ]
+  }
+}
