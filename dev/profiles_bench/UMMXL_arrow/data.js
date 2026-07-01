26caa9f1c8cc489192defa8702fd4b711cfc50e8
github-action-benchmark <github@users.noreply.github.com>
2026-07-01 10:19:59 +0000

    add GCodeAnalyzer UMMXL_arrow (customBiggerIsBetter) benchmark result for af7ab055df1cca155d001f724d81f0fc3c94cf91

--- dev/profiles_bench/UMMXL_arrow/data.js
+++ dev/profiles_bench/UMMXL_arrow/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1782898989967,
+  "lastUpdate": 1782901199214,
   "repoUrl": "https://github.com/Ultimaker/GCodeInsight",
   "entries": {
     "GCodeAnalyzer UMMXL_arrow": [
@@ -297,6 +297,98 @@ window.BENCHMARK_DATA = {
           "url": "https://github.com/Ultimaker/GCodeInsight/commit/3a0b6aa80dfcdadeda262c334baf5971d8abc9c9"
         },
         "date": 1782898973176,
+        "tool": "customBiggerIsBetter",
+        "benches": [
+          {
+            "name": "Print time UMMXL_arrow.gcode",
+            "value": 5014.778564874503,
+            "unit": "s"
+          },
+          {
+            "name": "Microsegment violations in wall-skin UMMXL_arrow.gcode",
+            "value": 0,
+            "unit": "-"
+          },
+          {
+            "name": "Microsegment violations in infill UMMXL_arrow.gcode",
+            "value": 0,
+            "unit": "-"
+          },
+          {
+            "name": "Number of retractions UMMXL_arrow.gcode",
+            "value": 1310,
+            "unit": "-"
+          },
+          {
+            "name": "Total travel length UMMXL_arrow.gcode",
+            "value": 14984.513224862869,
+            "unit": "mm"
+          },
+          {
+            "name": "Minimum Line Length UMMXL_arrow.gcode",
+            "value": 0.01699999999999946,
+            "unit": "mm"
+          },
+          {
+            "name": "Line Lengths 25 Percentile UMMXL_arrow.gcode",
+            "value": 0.676689736880941,
+            "unit": "mm"
+          },
+          {
+            "name": "Minimum All Distances UMMXL_arrow.gcode",
+            "value": 0.016000000000000014,
+            "unit": "mm"
+          },
+          {
+            "name": "All Distances 25 Percentile UMMXL_arrow.gcode",
+            "value": 0.3999999999999999,
+            "unit": "mm"
+          },
+          {
+            "name": "Extrusion-Axis UMMXL_arrow.gcode",
+            "value": 0.51375,
+            "unit": "mm"
+          },
+          {
+            "name": "Extrusion Lengths 25 Percentile UMMXL_arrow.gcode",
+            "value": 1051.62525,
+            "unit": "mm"
+          },
+          {
+            "name": "Number Of Temperature Commands UMMXL_arrow.gcode",
+            "value": 30331,
+            "unit": "#"
+          },
+          {
+            "name": "Mean Temperature UMMXL_arrow.gcode",
+            "value": 255,
+            "unit": "Celcius"
+          },
+          {
+            "name": "Slicing time UMMXL_arrow.gcode",
+            "value": 0,
+            "unit": "s"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "name": "wawanbreton",
+            "username": "wawanbreton",
+            "email": "601114+wawanbreton@users.noreply.github.com"
+          },
+          "committer": {
+            "name": "github-actions[bot]",
+            "username": "github-actions[bot]",
+            "email": "41898282+github-actions[bot]@users.noreply.github.com"
+          },
+          "id": "af7ab055df1cca155d001f724d81f0fc3c94cf91",
+          "message": "Run benchmark",
+          "timestamp": "2026-07-01T10:19:34Z",
+          "url": "https://github.com/Ultimaker/GCodeInsight/commit/af7ab055df1cca155d001f724d81f0fc3c94cf91"
+        },
+        "date": 1782901185536,
         "tool": "customBiggerIsBetter",
         "benches": [
           {
