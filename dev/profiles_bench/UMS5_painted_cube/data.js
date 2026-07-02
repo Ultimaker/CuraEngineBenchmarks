5c665d4e72659c79710f039adec003d582063bff
github-action-benchmark <github@users.noreply.github.com>
2026-07-02 12:23:07 +0000

    add GCodeAnalyzer UMS5_painted_cube (customBiggerIsBetter) benchmark result for 5eab76a821acbaa4e24a906a0137eaa937925c8c

--- dev/profiles_bench/UMS5_painted_cube/data.js
+++ dev/profiles_bench/UMS5_painted_cube/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1782905956670,
+  "lastUpdate": 1782994987922,
   "repoUrl": "https://github.com/Ultimaker/GCodeInsight",
   "entries": {
     "GCodeAnalyzer UMS5_painted_cube": [
@@ -551,6 +551,38 @@ window.BENCHMARK_DATA = {
           {
             "name": "Slicing time UMS5_painted_cube.gcode",
             "value": 0.358,
+            "unit": "s"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "name": "wawanbreton",
+            "username": "wawanbreton",
+            "email": "601114+wawanbreton@users.noreply.github.com"
+          },
+          "committer": {
+            "name": "github-actions[bot]",
+            "username": "github-actions[bot]",
+            "email": "41898282+github-actions[bot]@users.noreply.github.com"
+          },
+          "id": "5eab76a821acbaa4e24a906a0137eaa937925c8c",
+          "message": "Run benchmark",
+          "timestamp": "2026-07-02T12:21:57Z",
+          "url": "https://github.com/Ultimaker/GCodeInsight/commit/5eab76a821acbaa4e24a906a0137eaa937925c8c"
+        },
+        "date": 1782994973762,
+        "tool": "customBiggerIsBetter",
+        "benches": [
+          {
+            "name": "Buffer overflows",
+            "value": 2351,
+            "unit": "#"
+          },
+          {
+            "name": "Print time",
+            "value": 3441,
             "unit": "s"
           }
         ]
