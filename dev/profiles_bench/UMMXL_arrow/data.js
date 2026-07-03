d81c79fa4f192bdcda0456d83fabb3165bf8a2b9
github-action-benchmark <github@users.noreply.github.com>
2026-07-03 03:19:39 +0000

    add GCodeAnalyzer UMMXL_arrow (customBiggerIsBetter) benchmark result for 54e9c55350a4c426cd157d65533ac7bd26fb429f

--- dev/profiles_bench/UMMXL_arrow/data.js
+++ dev/profiles_bench/UMMXL_arrow/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1782998809615,
+  "lastUpdate": 1783048778935,
   "repoUrl": "https://github.com/Ultimaker/GCodeInsight",
   "entries": {
     "GCodeAnalyzer UMMXL_arrow": [
@@ -26,6 +26,43 @@ window.BENCHMARK_DATA = {
           {
             "name": "Slicing time",
             "value": 0.063,
+            "unit": "s"
+          },
+          {
+            "name": "Print time",
+            "value": 5140,
+            "unit": "s"
+          },
+          {
+            "name": "Buffer overflows",
+            "value": 4,
+            "unit": "#"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "name": "wawanbreton",
+            "username": "wawanbreton",
+            "email": "601114+wawanbreton@users.noreply.github.com"
+          },
+          "committer": {
+            "name": "github-actions[bot]",
+            "username": "github-actions[bot]",
+            "email": "41898282+github-actions[bot]@users.noreply.github.com"
+          },
+          "id": "54e9c55350a4c426cd157d65533ac7bd26fb429f",
+          "message": "Run benchmark",
+          "timestamp": "2026-07-03T03:18:48Z",
+          "url": "https://github.com/Ultimaker/GCodeInsight/commit/54e9c55350a4c426cd157d65533ac7bd26fb429f"
+        },
+        "date": 1783048765725,
+        "tool": "customBiggerIsBetter",
+        "benches": [
+          {
+            "name": "Slicing time",
+            "value": 0.064,
             "unit": "s"
           },
           {
