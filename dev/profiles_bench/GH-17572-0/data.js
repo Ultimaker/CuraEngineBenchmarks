5db838a724680b5423c2fd0e434caa76012719a1
github-action-benchmark <github@users.noreply.github.com>
2026-07-10 16:34:32 +0000

    add GCodeAnalyzer GH-17572-0 (customBiggerIsBetter) benchmark result for 673d4aa9bbe689e2b9fe9f93e6cc43d8ddbf5778

---
+++ dev/profiles_bench/GH-17572-0/data.js
@@ -0,0 +1,45 @@
+window.BENCHMARK_DATA = {
+  "lastUpdate": 1783701272660,
+  "repoUrl": "https://github.com/Ultimaker/GCodeInsight",
+  "entries": {
+    "GCodeAnalyzer GH-17572-0": [
+      {
+        "commit": {
+          "author": {
+            "name": "Erwan MATHIEU",
+            "username": "wawanbreton",
+            "email": "erwan.mathieu@ultimaker.com"
+          },
+          "committer": {
+            "name": "Erwan MATHIEU",
+            "username": "wawanbreton",
+            "email": "erwan.mathieu@ultimaker.com"
+          },
+          "id": "673d4aa9bbe689e2b9fe9f93e6cc43d8ddbf5778",
+          "message": "Fix gcode path not matching time file\n\nCURA-13226",
+          "timestamp": "2026-07-10T13:43:47Z",
+          "url": "https://github.com/Ultimaker/GCodeInsight/commit/673d4aa9bbe689e2b9fe9f93e6cc43d8ddbf5778"
+        },
+        "date": 1783701257776,
+        "tool": "customBiggerIsBetter",
+        "benches": [
+          {
+            "name": "Slicing time",
+            "value": 1.171,
+            "unit": "s"
+          },
+          {
+            "name": "Print time",
+            "value": 2155,
+            "unit": "s"
+          },
+          {
+            "name": "Buffer overflows",
+            "value": 81194,
+            "unit": "#"
+          }
+        ]
+      }
+    ]
+  }
+}
