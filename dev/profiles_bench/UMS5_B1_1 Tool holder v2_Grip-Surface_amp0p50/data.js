fd0ff4f72a504c094d0c7ea7ab1511d4cd8aecd7
github-action-benchmark <github@users.noreply.github.com>
2026-07-13 09:28:53 +0000

    add GCodeAnalyzer UMS5_B1_1 Tool holder v2_Grip-Surface_amp0p50 (customBiggerIsBetter) benchmark result for 6182450170166657579cd4f3c6b244d275db2cb0

---
+++ dev/profiles_bench/UMS5_B1_1 Tool holder v2_Grip-Surface_amp0p50/data.js
@@ -0,0 +1,45 @@
+window.BENCHMARK_DATA = {
+  "lastUpdate": 1783934933337,
+  "repoUrl": "https://github.com/Ultimaker/GCodeInsight",
+  "entries": {
+    "GCodeAnalyzer UMS5_B1_1 Tool holder v2_Grip-Surface_amp0p50": [
+      {
+        "commit": {
+          "author": {
+            "name": "Erwan MATHIEU",
+            "username": "wawanbreton",
+            "email": "erwan.mathieu@ultimaker.com"
+          },
+          "committer": {
+            "name": "Erwan MATHIEU",
+            "username": "wawanbreton",
+            "email": "erwan.mathieu@ultimaker.com"
+          },
+          "id": "6182450170166657579cd4f3c6b244d275db2cb0",
+          "message": "Properly handle files with spaces in name\n\nCURA-13226",
+          "timestamp": "2026-07-13T06:35:24Z",
+          "url": "https://github.com/Ultimaker/GCodeInsight/commit/6182450170166657579cd4f3c6b244d275db2cb0"
+        },
+        "date": 1783934917922,
+        "tool": "customBiggerIsBetter",
+        "benches": [
+          {
+            "name": "Slicing time",
+            "value": 213.981,
+            "unit": "s"
+          },
+          {
+            "name": "Print time",
+            "value": 22886,
+            "unit": "s"
+          },
+          {
+            "name": "Buffer overflows",
+            "value": 240632,
+            "unit": "#"
+          }
+        ]
+      }
+    ]
+  }
+}
