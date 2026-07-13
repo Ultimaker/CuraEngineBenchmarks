1a9eddf14b1af9816b41cc32ad191798746b2457
github-action-benchmark <github@users.noreply.github.com>
2026-07-13 08:00:25 +0000

    add GCodeAnalyzer GH-16901-0 (customBiggerIsBetter) benchmark result for 6182450170166657579cd4f3c6b244d275db2cb0

--- dev/profiles_bench/GH-16901-0/data.js
+++ dev/profiles_bench/GH-16901-0/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1783699001151,
+  "lastUpdate": 1783929625021,
   "repoUrl": "https://github.com/Ultimaker/GCodeInsight",
   "entries": {
     "GCodeAnalyzer GH-16901-0": [
@@ -21,6 +21,43 @@ window.BENCHMARK_DATA = {
           "url": "https://github.com/Ultimaker/GCodeInsight/commit/673d4aa9bbe689e2b9fe9f93e6cc43d8ddbf5778"
         },
         "date": 1783698987136,
+        "tool": "customBiggerIsBetter",
+        "benches": [
+          {
+            "name": "Slicing time",
+            "value": 0.066,
+            "unit": "s"
+          },
+          {
+            "name": "Print time",
+            "value": 0,
+            "unit": "s"
+          },
+          {
+            "name": "Buffer overflows",
+            "value": 0,
+            "unit": "#"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "name": "Erwan MATHIEU",
+            "username": "wawanbreton",
+            "email": "erwan.mathieu@ultimaker.com"
+          },
+          "committer": {
+            "name": "Erwan MATHIEU",
+            "username": "wawanbreton",
+            "email": "erwan.mathieu@ultimaker.com"
+          },
+          "id": "6182450170166657579cd4f3c6b244d275db2cb0",
+          "message": "Properly handle files with spaces in name\n\nCURA-13226",
+          "timestamp": "2026-07-13T06:35:24Z",
+          "url": "https://github.com/Ultimaker/GCodeInsight/commit/6182450170166657579cd4f3c6b244d275db2cb0"
+        },
+        "date": 1783929611527,
         "tool": "customBiggerIsBetter",
         "benches": [
           {
