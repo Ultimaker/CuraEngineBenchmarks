c6ba3f7120be7e75a6fa96e9d1751543ec1d1761
wawanbreton <601114+wawanbreton@users.noreply.github.com>
2026-07-15 16:44:30 +0200

    Publish new benchmark results

---
+++ dev/profiles_bench/UMMXL_arrow/data.js
@@ -0,0 +1,27 @@
+window.BENCHMARK_DATA = {
+  "lastUpdate": 1784126661741,
+  "entries": {
+    "GCodeAnalyzer UMMXL_arrow": [
+      {
+        "timestamp": "2026-07-15 16:44:21.741210",
+        "benches": [
+          {
+            "name": "Slicing time",
+            "unit": "s",
+            "value": 0.063
+          },
+          {
+            "name": "Print time",
+            "unit": "s",
+            "value": 5140
+          },
+          {
+            "name": "Buffer overflows",
+            "unit": "#",
+            "value": 4
+          }
+        ]
+      }
+    ]
+  }
+}
---
+++ dev/profiles_bench/UMS5_painted_cube/data.js
@@ -0,0 +1,27 @@
+window.BENCHMARK_DATA = {
+  "lastUpdate": 1784126664532,
+  "entries": {
+    "GCodeAnalyzer UMS5_painted_cube": [
+      {
+        "timestamp": "2026-07-15 16:44:24.532869",
+        "benches": [
+          {
+            "name": "Slicing time",
+            "unit": "s",
+            "value": 0.432
+          },
+          {
+            "name": "Print time",
+            "unit": "s",
+            "value": 3441
+          },
+          {
+            "name": "Buffer overflows",
+            "unit": "#",
+            "value": 2349
+          }
+        ]
+      }
+    ]
+  }
+}
