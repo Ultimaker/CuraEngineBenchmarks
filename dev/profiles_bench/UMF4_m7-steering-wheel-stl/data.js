dd42847d343093ed9fa4cd9c1ca190a89c20e6f5
github-action-benchmark <github@users.noreply.github.com>
2026-07-15 10:04:26 +0000

    add GCodeAnalyzer UMF4_m7-steering-wheel-stl (customBiggerIsBetter) benchmark result for 3459cc34e02ed6705a7c2fc5617fe49f76773c2c

--- dev/profiles_bench/UMF4_m7-steering-wheel-stl/data.js
+++ dev/profiles_bench/UMF4_m7-steering-wheel-stl/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1784048139106,
+  "lastUpdate": 1784109866542,
   "repoUrl": "https://github.com/Ultimaker/GCodeInsight",
   "entries": {
     "GCodeAnalyzer UMF4_m7-steering-wheel-stl": [
@@ -369,6 +369,43 @@ window.BENCHMARK_DATA = {
           {
             "name": "Buffer overflows",
             "value": 415147,
+            "unit": "#"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "name": "wawanbreton",
+            "username": "wawanbreton",
+            "email": "601114+wawanbreton@users.noreply.github.com"
+          },
+          "committer": {
+            "name": "github-actions[bot]",
+            "username": "github-actions[bot]",
+            "email": "41898282+github-actions[bot]@users.noreply.github.com"
+          },
+          "id": "3459cc34e02ed6705a7c2fc5617fe49f76773c2c",
+          "message": "Run benchmark",
+          "timestamp": "2026-07-15T08:31:19Z",
+          "url": "https://github.com/Ultimaker/GCodeInsight/commit/3459cc34e02ed6705a7c2fc5617fe49f76773c2c"
+        },
+        "date": 1784109853126,
+        "tool": "customBiggerIsBetter",
+        "benches": [
+          {
+            "name": "Slicing time",
+            "value": 360.147,
+            "unit": "s"
+          },
+          {
+            "name": "Print time",
+            "value": 71933,
+            "unit": "s"
+          },
+          {
+            "name": "Buffer overflows",
+            "value": 415296,
             "unit": "#"
           }
         ]
