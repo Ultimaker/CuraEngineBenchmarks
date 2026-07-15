e831ff7b4f9506b97e8bbac685161d7afe3c13c3
github-action-benchmark <github@users.noreply.github.com>
2026-07-15 08:51:30 +0000

    add GCodeAnalyzer GH-17229-0 (customBiggerIsBetter) benchmark result for 3459cc34e02ed6705a7c2fc5617fe49f76773c2c

--- dev/profiles_bench/GH-17229-0/data.js
+++ dev/profiles_bench/GH-17229-0/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1784043613781,
+  "lastUpdate": 1784105490675,
   "repoUrl": "https://github.com/Ultimaker/GCodeInsight",
   "entries": {
     "GCodeAnalyzer GH-17229-0": [
@@ -100,6 +100,43 @@ window.BENCHMARK_DATA = {
           {
             "name": "Slicing time",
             "value": 0.402,
+            "unit": "s"
+          },
+          {
+            "name": "Print time",
+            "value": 0,
+            "unit": "s"
+          },
+          {
+            "name": "Buffer overflows",
+            "value": 41224,
+            "unit": "#"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "name": "wawanbreton",
+            "username": "wawanbreton",
+            "email": "601114+wawanbreton@users.noreply.github.com"
+          },
+          "committer": {
+            "name": "github-actions[bot]",
+            "username": "github-actions[bot]",
+            "email": "41898282+github-actions[bot]@users.noreply.github.com"
+          },
+          "id": "3459cc34e02ed6705a7c2fc5617fe49f76773c2c",
+          "message": "Run benchmark",
+          "timestamp": "2026-07-15T08:31:19Z",
+          "url": "https://github.com/Ultimaker/GCodeInsight/commit/3459cc34e02ed6705a7c2fc5617fe49f76773c2c"
+        },
+        "date": 1784105477159,
+        "tool": "customBiggerIsBetter",
+        "benches": [
+          {
+            "name": "Slicing time",
+            "value": 0.4,
             "unit": "s"
           },
           {
