2be203464c3a8663c1703990a13e8ec16bbee954
github-action-benchmark <github@users.noreply.github.com>
2026-07-15 10:04:02 +0000

    add GCodeAnalyzer UMDLL_TRANSFERT (customBiggerIsBetter) benchmark result for 3459cc34e02ed6705a7c2fc5617fe49f76773c2c

--- dev/profiles_bench/UMDLL_TRANSFERT/data.js
+++ dev/profiles_bench/UMDLL_TRANSFERT/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1784048112707,
+  "lastUpdate": 1784109842892,
   "repoUrl": "https://github.com/Ultimaker/GCodeInsight",
   "entries": {
     "GCodeAnalyzer UMDLL_TRANSFERT": [
@@ -110,6 +110,43 @@ window.BENCHMARK_DATA = {
           {
             "name": "Buffer overflows",
             "value": 2034967,
+            "unit": "#"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "name": "wawanbreton",
+            "username": "wawanbreton",
+            "email": "601114+wawanbreton@users.noreply.github.com"
+          },
+          "committer": {
+            "name": "github-actions[bot]",
+            "username": "github-actions[bot]",
+            "email": "41898282+github-actions[bot]@users.noreply.github.com"
+          },
+          "id": "3459cc34e02ed6705a7c2fc5617fe49f76773c2c",
+          "message": "Run benchmark",
+          "timestamp": "2026-07-15T08:31:19Z",
+          "url": "https://github.com/Ultimaker/GCodeInsight/commit/3459cc34e02ed6705a7c2fc5617fe49f76773c2c"
+        },
+        "date": 1784109828336,
+        "tool": "customBiggerIsBetter",
+        "benches": [
+          {
+            "name": "Slicing time",
+            "value": 92.97800000000001,
+            "unit": "s"
+          },
+          {
+            "name": "Print time",
+            "value": 56856,
+            "unit": "s"
+          },
+          {
+            "name": "Buffer overflows",
+            "value": 2034952,
             "unit": "#"
           }
         ]
