43ca80f896e0776bddc8dc7175ac87ac5833872f
github-action-benchmark <github@users.noreply.github.com>
2026-07-20 14:23:46 +0000

    add Stress Benchmark (customSmallerIsBetter) benchmark result for 10f1b81d55f0c5615627d9a80cb74cdc1bf23887

--- dev/stress_bench/data.js
+++ dev/stress_bench/data.js
@@ -1,40 +1,8 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1784540494555,
+  "lastUpdate": 1784557426035,
   "repoUrl": "https://github.com/Ultimaker/CuraEngine",
   "entries": {
     "Stress Benchmark": [
-      {
-        "commit": {
-          "author": {
-            "name": "Ultimaker",
-            "username": "Ultimaker"
-          },
-          "committer": {
-            "name": "Ultimaker",
-            "username": "Ultimaker"
-          },
-          "id": "b9cabf297d1c1c1fbc31c1f4b1358b7415f2e6ef",
-          "message": "CURA-12957 infill can leave small dots lines",
-          "timestamp": "2026-02-13T07:28:28Z",
-          "url": "https://github.com/Ultimaker/CuraEngine/pull/2283/commits/b9cabf297d1c1c1fbc31c1f4b1358b7415f2e6ef"
-        },
-        "date": 1770987156818,
-        "tool": "customSmallerIsBetter",
-        "benches": [
-          {
-            "name": "Number of test cases",
-            "value": 61,
-            "unit": "-",
-            "extra": ""
-          },
-          {
-            "name": "General Stress Level",
-            "value": 0,
-            "unit": "%",
-            "extra": "Crashes in: "
-          }
-        ]
-      },
       {
         "commit": {
           "author": {
@@ -8023,6 +7991,38 @@ window.BENCHMARK_DATA = {
           "url": "https://github.com/Ultimaker/CuraEngine/pull/2349/commits/93f5d54c32156f2f5e8144401639e002a5d88a82"
         },
         "date": 1784540485886,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "Number of test cases",
+            "value": 61,
+            "unit": "-",
+            "extra": ""
+          },
+          {
+            "name": "General Stress Level",
+            "value": 0,
+            "unit": "%",
+            "extra": "Crashes in: "
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "name": "Ultimaker",
+            "username": "Ultimaker"
+          },
+          "committer": {
+            "name": "Ultimaker",
+            "username": "Ultimaker"
+          },
+          "id": "10f1b81d55f0c5615627d9a80cb74cdc1bf23887",
+          "message": "CURA-13250 chamfered tree base",
+          "timestamp": "2026-07-20T11:09:30Z",
+          "url": "https://github.com/Ultimaker/CuraEngine/pull/2346/commits/10f1b81d55f0c5615627d9a80cb74cdc1bf23887"
+        },
+        "date": 1784557417548,
         "tool": "customSmallerIsBetter",
         "benches": [
           {
